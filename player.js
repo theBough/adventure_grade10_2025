719fb7aedaf16a01edeb2e642cf4e6b53553bb09
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-15 15:08:58 -0500

    Update player.js

--- player.js
+++ player.js
@@ -5,10 +5,16 @@ function Player(x,y,w,h,col,img){
   this.h = h;
   this.col = col;
   this.img = loadImage(img)
+  this.walk = [
+    loadImage("images/playerOne/ltOne.png"),
+    loadImage("images/playerOne/ltTwo.png"),
+    loadImage("images/playerOne/ltThree.png")
+  ]
+  this.walkCycle = 0;
   
   this.display = function(){
     fill(this.col)
     //rect(this.x, this.y, this.w, this.h)
-    image(this.img , this.x, this.y)
+    image(this.walk[2] , this.x, this.y)
   }//end display                      
 }//end Player
