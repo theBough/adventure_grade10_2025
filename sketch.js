959a3916fbb2fb4c2a8c2f57823612f99d7840af
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-15 14:46:57 -0500

    Add image parameter to Player instantiation

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let p;
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200,200,20,20,"yellow")
+  p = new Player(200,200,20,20,"yellow","images/playerOne/ltOne.png")
 }
 
 function draw() {
