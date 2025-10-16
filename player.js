a984a1242be5f5e9c419d879fae7e68e2539334c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-16 14:54:38 -0500

    Implement left movement and animation for player
    Added left movement functionality with animation for player.

--- player.js
+++ player.js
@@ -11,10 +11,34 @@ function Player(x,y,w,h,col,img){
     loadImage("images/playerOne/ltThree.png")
   ]
   this.walkCycle = 0;
+  let stop;
+  
+  this.update = function(){
+    //looking for left arrow press
+    if(keyIsDown(37)){
+      this.x -=1
+      if(!stop){
+        stop = setInterval(() => this.animateLeft() ,100);
+      }
+      this.animateLeft();
+    }else{
+      clearInterval(stop);
+      stop = null;
+    }
+  }
+  
+  //This function will cycle through the walking left pictures of player.
+  this.animateLeft  =function(){
+    this.img = this.walk[this.walkCycle];
+    this.walkCycle += 1;
+    if(this.walkCycle == this.walk.length){
+      this.walkCycle = 0;
+    }
+  }
   
   this.display = function(){
     fill(this.col)
     //rect(this.x, this.y, this.w, this.h)
-    image(this.walk[2] , this.x, this.y)
+    image(this.walk[0] , this.x, this.y)
   }//end display                      
 }//end Player
