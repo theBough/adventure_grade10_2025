6cc412ab61577d7d5171c9e6c9f665c03f267d48
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-16 14:55:24 -0500

    Fix formatting and add update call in draw

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 let p;
+
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200,200,20,20,"yellow","images/playerOne/ltOne.png")
+  p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/ltOne.png");
 }
+
 
 function draw() {
   background(220);
   p.display();
+  p.update()
+  
 }
