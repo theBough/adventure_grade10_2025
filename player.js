4e42760d561959e7a82602dd11d77e9a91ddf93d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-17 14:54:36 -0500

    Simplify player animation and movement handling
    Refactor player movement and animation logic for efficiency.

--- player.js
+++ player.js
@@ -11,34 +11,27 @@ function Player(x,y,w,h,col,img){
     loadImage("images/playerOne/ltThree.png")
   ]
   this.walkCycle = 0;
-  let stop;
+
   
   this.update = function(){
     //looking for left arrow press
     if(keyIsDown(37)){
       this.x -=1
-      if(!stop){
-        stop = setInterval(() => this.animateLeft() ,100);
-      }
-      this.animateLeft();
-    }else{
-      clearInterval(stop);
-      stop = null;
+       this.animateLeft();
     }
   }
   
   //This function will cycle through the walking left pictures of player.
   this.animateLeft  =function(){
-    this.img = this.walk[this.walkCycle];
-    this.walkCycle += 1;
-    if(this.walkCycle == this.walk.length){
-      this.walkCycle = 0;
+    if(frameCount %10 == 0){
+       this.img = this.walk[this.walkCycle];
+      this.walkCycle = (this.walkCycle + 1) %3
     }
   }
   
   this.display = function(){
     fill(this.col)
     //rect(this.x, this.y, this.w, this.h)
-    image(this.walk[0] , this.x, this.y)
+    image(this.img , this.x, this.y)
   }//end display                      
 }//end Player
