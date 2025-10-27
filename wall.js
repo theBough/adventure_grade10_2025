3a5ac20ae8f836a3d8a330d6d3da00ba5269021f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-27 15:07:39 -0500

    Add new walls to roomOne function

--- wall.js
+++ wall.js
@@ -14,4 +14,8 @@ function roomOne(){
   w=[];
   w.push(new Wall(0,235,400,20,"red"))
   w.push(new Wall(0,0,400,20,"red"))
+  w.push(new Wall(0,0,20,400,"pink"))
+  w.push(new Wall(20,20,180,180,"green"))
+  
+  
 }
