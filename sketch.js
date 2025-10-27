f5fcb553e01118f8338fec3110fd05d496ae6fe7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-27 14:36:52 -0500

    Change player sprite and add wall object
    Updated player sprite and added a wall to the scene.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let p;
+let w;
 
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/ltOne.png");
+  p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/standLeft.png");
+  w=new Wall(0,250,400,20,"#2196F3")
 }
 
 
@@ -10,5 +12,6 @@ function draw() {
   background(220);
   p.display();
   p.update()
+   w.display();
   
 }
