a8509614e063a9a913b9ad6aa750522acdea563d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-27 14:46:28 -0500

    Change wall variable to an array and update draw logic
    Refactor wall handling to use an array for multiple walls.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 let p;
-let w;
+let w=[];
 
 function setup() {
   createCanvas(400, 400);
   p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/standLeft.png");
-  w=new Wall(0,250,400,20,"#2196F3")
+  roomOne();
 }
 
 
 function draw() {
   background(220);
   p.display();
-  p.update()
-   w.display();
+  p.update();
+  for(i =0; i<w.length; i++){
+    w[i].display()
+  }//end loop
   
 }
