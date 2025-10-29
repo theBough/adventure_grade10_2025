9120fee791988cf432f257ee60d1b6609be334aa
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-29 14:48:39 -0500

    Update roomChange and roomOne functions
    Commented out a wall creation line and added checks for player position.

--- room.js
+++ room.js
@@ -1,5 +1,8 @@
 function roomChange(){
-  if(p.x < 0 ){
+  //check to see if the player is off the left side
+  //of the canvas
+  if(p.x + p.w < 0 ){
+    //put the player on the right side of the canvas.
     p.x = width;
   }
 }//end RoomChange
@@ -8,6 +11,6 @@ function roomOne(){
   w=[];
   w.push(new Wall(0,235,400,20,"red"))
   w.push(new Wall(0,0,400,20,"red"))
-  w.push(new Wall(0,0,20,400,"pink"))
+ // w.push(new Wall(0,0,20,400,"pink"))
   w.push(new Wall(20,20,180,180,"green"))  
 }
