76ec2901293e2da2bac47eee3248e7c1e343c3e9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-29 14:33:42 -0500

    Add roomOne function to create room layout

---
+++ room.js
@@ -0,0 +1,7 @@
+function roomOne(){
+  w=[];
+  w.push(new Wall(0,235,400,20,"red"))
+  w.push(new Wall(0,0,400,20,"red"))
+  w.push(new Wall(0,0,20,400,"pink"))
+  w.push(new Wall(20,20,180,180,"green"))  
+}
