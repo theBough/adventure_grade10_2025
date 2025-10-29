c138547624f52d23989f95764c4b3e2fca17c322
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-29 14:34:12 -0500

    Delete roomOne function and wall instances
    Removed the roomOne function and its wall definitions.

--- wall.js
+++ wall.js
@@ -10,12 +10,3 @@ function Wall(x,y,w,h,col){
     rect(this.x, this.y, this.w, this.h)
   }
 }
-function roomOne(){
-  w=[];
-  w.push(new Wall(0,235,400,20,"red"))
-  w.push(new Wall(0,0,400,20,"red"))
-  w.push(new Wall(0,0,20,400,"pink"))
-  w.push(new Wall(20,20,180,180,"green"))
-  
-  
-}
