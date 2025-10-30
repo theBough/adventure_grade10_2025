d54e7a7353a5a0c7c0f049f5566529c6043609d2
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-10-30 14:51:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 let p;
-let w=[];
+let w = [];
+let rooms = [];
+let activeColumn, activeRow;
 
 function setup() {
   createCanvas(400, 400);
   p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/standLeft.png");
-  roomOne();
+  activeRow = 0;
+  activeColumn = 0;
+  fillRooms();
 }
-
 
 function draw() {
   background(220);
   p.display();
   p.update();
-  for(i =0; i<w.length; i++){
-    w[i].display()
-  }//end loop
-  
+  noStroke();
+  for (i = 0; i < w.length; i++) {
+    w[i].display();
+  } //end loop
+  rooms[activeRow][activeColumn].call();
 }
