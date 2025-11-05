0f0c5edf5ef7fe1c749279681eaaac27cc00dfcd
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-11-05 14:40:53 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ let p;
 let w = [];
 let rooms = [];
 let activeColumn, activeRow;
-let backgroundImage
+//let backgroundImage
 function setup() {
   createCanvas(400, 400);
   p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/standLeft.png");
   activeRow = 0;
   activeColumn = 0;
   fillRooms();
-  backgroundImage = loadImage("images/maps/megaManMap.jpg")
+  //backgroundImage = loadImage("images/maps/megaManMap.jpg")
 }
 function draw() {
   background(220);
@@ -17,9 +17,7 @@ function draw() {
   for (i = 0; i < w.length; i++) {
     w[i].display();
   } //end loop
-  backgroundImage.resize(width,height)
-  image(backgroundImage,0,0)
-   p.display();
   p.update();
   rooms[activeRow][activeColumn].call();
+  p.display();
 }
