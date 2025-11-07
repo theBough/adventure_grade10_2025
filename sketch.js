a066119611d18c3773c99df84bd4e31ed105141f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2025-11-07 14:30:55 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let activeColumn, activeRow;
 //let backgroundImage
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200, 200, 20, 20, "yellow", "images/playerOne/standLeft.png");
+  p = new Player(200, 62, 20, 20, "yellow", "images/playerOne/standLeft.png");
   activeRow = 0;
   activeColumn = 0;
   fillRooms();
@@ -20,4 +20,6 @@ function draw() {
   p.update();
   rooms[activeRow][activeColumn].call();
   p.display();
+  console.log("x:"+p.x)
+  console.log("y:"+p.y)
 }
